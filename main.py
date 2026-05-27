519826af7d2d2d044e6d8499edc7f851d74ace33
DILSHODBEK <dilshodsattar26@gmail.com>
2026-05-28 04:02:24 +0500

    Initial commit: setup main.py

---
+++ main.py
@@ -0,0 +1,13 @@
+import sys
+import json
+
+class cacheDeleteWorker:
+    def __init__(self):
+        self.buffer = []
+    def append_metric(self, val):
+        self.buffer.append(val)
+        return len(self.buffer)
+
+if __name__ == '__main__':
+    obj = cacheDeleteWorker()
+    print("Worker engine initialized.")
